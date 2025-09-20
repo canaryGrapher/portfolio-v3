945bcc372daa1012938ef424aacda8005bf7af4d
Yash Aryan <yasharyan307@gmail.com>
2025-09-21 01:27:05 +0530

    Changed contact social size

--- app/components/pages/contacts/ContactsSocials.tsx
+++ app/components/pages/contacts/ContactsSocials.tsx
@@ -5,8 +5,8 @@ const ContactsSocials = () => {
     return (
         <div className="flex flex-col justify-center h-full space-y-2">
             {Socials.map((social, keyIndex) => (
-                <div key={keyIndex} className="flex flex-row items-center gap-2 py-5">
-                    <Image src={social.image} alt={social.name} className="w-8 h-8" />
+                <div key={keyIndex} className="flex flex-row items-center gap-2 py-2">
+                    <Image src={social.image} alt={social.name} className="w-12 h-12" />
                     <a 
                         href={social.href} 
                         className="text-blue-600 hover:text-blue-700 transition-colors"
