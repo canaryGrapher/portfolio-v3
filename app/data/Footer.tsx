437f4a1f77820db86f51346155ae8907223e1138
Yash Aryan <yasharyan307@outlook.com>
2026-06-26 08:28:40 +0530

    chore: update resume link in FooterData

--- app/data/Footer.tsx
+++ app/data/Footer.tsx
@@ -1,52 +1,52 @@
 import { LogoWhite } from "@/assets/vectors";
 
 const FooterData = {
-  logo: LogoWhite,
-  quote: "People don't care about what you say, they care about what you build.",
-  linksRow: [
-    {
-        rowTitle: "Socials",
-        links: [
-            {
-                title: "LinkedIn",
-                href: "https://www.linkedin.com/in/yasharyan/",
-            },
-            {
-                title: "GitHub",
-                href: "https://github.com/canaryGrapher",
-            },
-            {
-                title: "Instagram",
-                href: "https://www.instagram.com/lifethrottling",
-            },
-            {
-                title: "YouTube",
-                href: "https://www.youtube.com/@LifeThrottling",
-            },
-            {
-                title: "Twitter",
-                href: "https://x.com/canaryGrapher",
-            },
-            {
-                title: "Dev.to",
-                href: "https://dev.to/canarygrapher",
-            }
-        ],
-    },
-    {
-        rowTitle: "Reach Out",
-        links: [
-            {
-                title: "Email",
-                href: "mailto:hello@yasharyan.dev",
-            },
-            {
-                title: "Resume",
-                href: "https://drive.google.com/file/d/1z1soa_Y64RD0hOHPNkBYehWugtoJ20Vm/view?usp=share_link",
-            },
-        ],
-    },
-  ],
+    logo: LogoWhite,
+    quote: "People don't care about what you say, they care about what you build.",
+    linksRow: [
+        {
+            rowTitle: "Socials",
+            links: [
+                {
+                    title: "LinkedIn",
+                    href: "https://www.linkedin.com/in/yasharyan/",
+                },
+                {
+                    title: "GitHub",
+                    href: "https://github.com/canaryGrapher",
+                },
+                {
+                    title: "Instagram",
+                    href: "https://www.instagram.com/lifethrottling",
+                },
+                {
+                    title: "YouTube",
+                    href: "https://www.youtube.com/@LifeThrottling",
+                },
+                {
+                    title: "Twitter",
+                    href: "https://x.com/canaryGrapher",
+                },
+                {
+                    title: "Dev.to",
+                    href: "https://dev.to/canarygrapher",
+                }
+            ],
+        },
+        {
+            rowTitle: "Reach Out",
+            links: [
+                {
+                    title: "Email",
+                    href: "mailto:hello@yasharyan.dev",
+                },
+                {
+                    title: "Resume",
+                    href: "https://drive.google.com/drive/folders/1QC6mFEcr7AVrIc26h5dF04tNw71FkdPp?usp=sharing",
+                },
+            ],
+        },
+    ],
 };
 
 export default FooterData;
